1fbf4950f2e11166d4356e178deb731d1bb3d1ee
Mette Munch <meha0005@stud.ek.dk>
2025-09-21 13:45:45 +0200

    Final commit after linter

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const app = express();
 // Sørger for at alle filer fra mappen "public" indlæses, både css og html
 app.use(express.static("public"));
 
-
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
